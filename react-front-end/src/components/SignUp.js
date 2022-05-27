767d2658baa212ca394a1e0d967b3e63038839d6
RiazK <RiazCKhan@gmail.com>
2022-05-27 10:41:52 -0400

    remove category pic display

--- react-front-end/src/components/SignUp.js
+++ react-front-end/src/components/SignUp.js
@@ -7,7 +7,6 @@ export default function SignUp() {
     <>
       <section>
 
-
         <div className="Slogan d-flex flex-column align-items-center">
           <h1 className="Signup_Slogan">WORK</h1>
           <br></br>
@@ -19,54 +18,6 @@ export default function SignUp() {
         <div className=" d-flex SignUp_shadow Slogan_pic">
           <img src="/cover2.jpeg" alt="" className="SignUpTopImg" />
         </div>
-
-        {/* <div className="categoriesWrapper padding-50 purpleBckgnd">
-          <div className="signCategories">
-            <img src="/back.jpeg" alt="" />
-            <div className="title topleftSM">Back</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/cardio.jpeg" alt="" />
-            <div className="title topleftSM">Cardio</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/chest.jpeg" alt="" />
-            <div className="title topleftSM">Chest</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/lower_arm.jpeg" alt="" />
-            <div className="title topleft">Lower Arms</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/lower_leg.jpeg" alt="" />
-            <div className="title topleft">Lower Legs</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/shoulder.jpeg" alt="" />
-            <div className="title topleft">Shoulders</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/upper_arm.jpeg" alt="" />
-            <div className="title topleft">Upper Arms</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/upper_leg.jpeg" alt="" />
-            <div className="title topleft">Upper Legs</div>
-          </div>
-
-          <div className="signCategories">
-            <img src="/core.jpeg" alt="" />
-            <div className="title topleftSM">Core</div>
-          </div>
-        </div> */}
-
 
       </section>
 
